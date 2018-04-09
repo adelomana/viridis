4471c2fdc7b4af91da48226185e0738a9fc43074
Adrian de Lomana <adrianlopezgarciadelomana@gmail.com>
2018-04-09 12:03:26 -0700

    final version of uvr and ccm analysis

--- transcriptomeAnalysis/delta.delta.analysis.py
+++ transcriptomeAnalysis/delta.delta.analysis.py
@@ -1,6 +1,5 @@
 ###
-### This script compares the UVR-response genes in HC vs LC.
-### It compares if there is consistent expression difference in epoch 0 vs 1 with respect to HC and LC on UVR-response genes.
+### This script compares differences of Stage 2 vs Stage 1 on carbon difference for sets of genes, e.g., UVR-response genes and CCMs.
 ###
 
 import sys,numpy,os,pickle
@@ -11,14 +10,14 @@ import library
 matplotlib.rcParams.update({'font.size':18,'font.family':'Arial','xtick.labelsize':14,'ytick.labelsize':14})
 matplotlib.rcParams['pdf.fonttype']=42
 
-def analysis(uvrGenes,verbose,plotting):
+def analysis(selectedSet,plotting):
 
     '''
-    This function computes the difference between Stage 2 and Stage 1 with respect to HC/LC fold change for UVR-response genes and others.
+    This function computes the difference between Stage 2 and Stage 1 with respect to HC/LC fold change for a selected set of genes.
     '''
 
     # f.1. define variables to return
-    effectsUVR=[]; effectsNOUVR=[]; effectsUVRdict={}
+    effectsSS=[]; effectsNOSS=[]; effectsSSdict={}
 
     # f.2. iterate over genes
     for geneName in sortedGeneNames:
@@ -60,6 +59,9 @@ def analysis(uvrGenes,verbose,plotting):
         mLC=numpy.mean(LC,axis=1)
         mHC=numpy.mean(HC,axis=1)
 
+        stdDevLC=numpy.std(LC,axis=1)
+        stdDevHC=numpy.std(HC,axis=1)
+
         # f.2.4. discard any gene whose max expression does not reach 10 FPKM
         top=max([max(mLC),max(mHC)])
         if top < 10+1:
@@ -77,12 +79,12 @@ def analysis(uvrGenes,verbose,plotting):
             effect=(sum(deltaStage2)/4)-(sum(deltaStage1)/4)
 
             # f.2.5. sort effects based on being UVR response genes or not
-            if geneName in uvrGenes:
-                effectsUVR.append(effect)
-                effectsUVRdict[geneName]=effect
+            if geneName in selectedSet:
+                effectsSS.append(effect)
+                effectsSSdict[geneName]=effect
                 # plotting if required
                 if plotting == True:
-                    figureFileName='figures/uvr.trajectory.{}.pdf'.format(geneName)
+                    figureFileName='figures/{}.trajectory.{}.pdf'.format(flag,geneName)
                     xTickLabels=['exp.AM','exp.PM','sta.AM','sta.PM','exp.AM','exp.PM','sta.AM','sta.PM']
                     x=[i for i in range(len(xTickLabels))]
                     matplotlib.pyplot.plot(x[:4],mLC[:4],':',color='blue',lw=2,label='LC')
@@ -100,14 +102,9 @@ def analysis(uvrGenes,verbose,plotting):
                     matplotlib.pyplot.savefig(figureFileName)
                     matplotlib.pyplot.clf()
             else:
-               effectsNOUVR.append(effect)
-
-    # f.3. print top 5 UVR effects
-    if verbose == True:
-        for element in sorted(effectsUVRdict,key=effectsUVRdict.get,reverse=True)[:5]:
-            print('\t{}\t{}'.format(element,effectsUVRdict[element]))
+               effectsNOSS.append(effect)
     
-    return effectsUVR,effectsNOUVR,effectsUVRdict
+    return effectsSS,effectsNOSS,effectsSSdict
 
 def crossValidation():
 
@@ -116,7 +113,7 @@ def crossValidation():
     '''
 
     # f.1. check for right jar, if not found, run a crossvalidation
-    rightJar='uvr.crossvalidation.{}.iterations.pckl'.format(crossValidationIterations)
+    rightJar='{}.crossvalidation.{}.iterations.pckl'.format(flag,crossValidationIterations)
     if os.path.exists(rightJar) == True:
         # f.1.1. open the jar
         f=open(rightJar,'rb')
@@ -137,45 +134,57 @@ def crossValidation():
 
     # f.2. plot a histogram
     H=numpy.array(hits)
-    pvalue=len(H[H>3])/len(H)
+    pvalue=len(H[H>numberOfDeviations])/len(H)
     print('\t crossvalidation p-value: {}'.format(pvalue))
     
-    x,y=histogrammer(hits)
-    cdfx=[]; cdfy=[]
-    for i in range(len(x)):
-        if y[i] != 0:
-            cdfx.append(x[i])
-            cdfy.append(y[i])
-    print(numpy.cumsum(cdfy))
-    matplotlib.pyplot.plot(cdfx,numpy.cumsum(cdfy),'o',mew=0,color='black')
-    matplotlib.pyplot.axvline(x=3,color='red',ls=':',lw=2)
+    x,y=histogrammer(hits,discrete=True)
+    cdf=numpy.cumsum(y)
+
+    print('\t cummulative distribution:')
+    for i in range(len(cdf)):
+        print('\t\t {} {}'.format(x[i],cdf[i]))
+
+    matplotlib.pyplot.plot(x,cdf,'o',mew=0,color='black')
+    print(numberOfDeviation)
+    matplotlib.pyplot.axvline(x=numberOfDeviations,color='red',ls=':',lw=2)
     
     matplotlib.pyplot.xlabel('Outlier rank')
     matplotlib.pyplot.ylabel('Cumulative probability')
     
     matplotlib.pyplot.tight_layout()
-    matplotlib.pyplot.savefig('uvr.crossvalidation.pdf')
-    matplotlib.pyplot.clf()          
-    return
+    matplotlib.pyplot.savefig('{}.crossvalidation.pdf'.format(flag))
+    matplotlib.pyplot.clf()
+    
+    return None
 
-def histogrammer(theData):
+def histogrammer(theData,discrete):
 
     '''
     This function creates a histogram.
     '''
 
-    n,bins=numpy.histogram(theData,bins=int(numpy.sqrt(len(theData))))
+    x=[]; y=[]
+    
+    if discrete == True:
+        a=min(theData); b=max(theData); c=len(theData)
+        for position in range(a,b+1):
+            x.append(position)
+            y.append(theData.count(position)/c)
 
-    x=[]
-    halfBin=(bins[1]-bins[0])/2.
-    for bin in bins:
-        center=bin+halfBin
-        x.append(center)
-    x.pop()
-
-    y=[]
-    y=numpy.array(n)
-    y=list(y/float(sum(y)))
+    elif discrete == False:
+        n,bins=numpy.histogram(theData,bins=int(numpy.sqrt(len(theData))))
+        
+        halfBin=(bins[1]-bins[0])/2.
+        for bin in bins:
+            center=bin+halfBin
+            x.append(center)
+        x.pop()
+        y=numpy.array(n)
+        y=list(y/float(sum(y)))
+        
+    else:
+        print('error from histogrammer to select discrete distribution...')
+        sys.exit()
 
     return x,y
 
@@ -185,15 +194,15 @@ def outlierFinder(tempo):
     This function is called by crossValidation and calls analysis to detect the number of outliers.
     '''
 
-    # f.1. define a set of random elements from all genes the same size of UVR set
-    randomSet=numpy.random.choice(sortedGeneNames,size=len(uvrGenes))
+    # f.1. define a set of random elements from all genes the same size of selected set
+    randomSet=numpy.random.choice(sortedGeneNames,size=len(selectedSet))
 
     # f.2. define how many of them pass a threshold
     outlierRank=0
-    tempo1,tempo2,effectsRandomSetdict=analysis(randomSet,verbose=False)
+    tempo1,tempo2,effectsRandomSetdict=analysis(randomSet,plotting=False)
     for geneName in effectsRandomSetdict:
         effect=effectsRandomSetdict[geneName]
-        if effect > topThreshold:
+        if effect > topThreshold or effect < bottomThreshold:
             outlierRank=outlierRank+1
 
     return outlierRank
@@ -205,10 +214,9 @@ def plotter():
     '''
 
     fig, (axTop, axBottom) = matplotlib.pyplot.subplots(2,1, gridspec_kw = {'height_ratios':[9, 1]})
-    #fig.subplots_adjust(hspace=0.03)
     
     # f.1. plot distribution of all genes
-    x,y=histogrammer(effectsNOUVR)
+    x,y=histogrammer(effectsNOSS,discrete=False)
     
     left=[[],[]]; center=[[],[]]; right=[[],[]]
     centers=[]
@@ -230,10 +238,10 @@ def plotter():
     print('\t thresholds: {} {}'.format(bottom,top))
 
     axTop.plot([top,top],[0,0.08],ls=':',color='red',lw=1)
-    axTop.plot([bottom,bottom],[0,0.08],ls=':',color='red',lw=1)
+    axTop.plot([bottom,bottom],[0,0.08],ls=':',color='blue',lw=1)
 
-    axTop.plot(left[0],left[1],'-',color='red',lw=1)
-    axTop.fill_between(left[0],left[1],numpy.zeros(len(left[1])),facecolor='red',alpha=0.2,edgecolor='None')
+    axTop.plot(left[0],left[1],'-',color='blue',lw=1)
+    axTop.fill_between(left[0],left[1],numpy.zeros(len(left[1])),facecolor='blue',alpha=0.2,edgecolor='None')
     
     axTop.plot(center[0],center[1],'-',color='black',lw=1)
     axTop.fill_between(center[0],center[1],numpy.zeros(len(center[1])),facecolor='black',alpha=0.2,edgecolor='None')
@@ -245,11 +253,13 @@ def plotter():
     axTop.set_xlim([-3.5,3.5])
     axTop.set_ylabel('Probability density function')
 
-    # f.2. plotting UVR genes
-    x,y=histogrammer(effectsUVR)
-    for element in effectsUVR:
+    # f.2. plotting SS genes
+    x,y=histogrammer(effectsSS,discrete=False)
+    for element in effectsSS:
         if element > top:
             axBottom.plot([element],[-0.01],'o',color='red',alpha=0.5,mew=0)
+        elif element < bottom:
+            axBottom.plot([element],[-0.01],'o',color='blue',alpha=0.5,mew=0)
         else:
             axBottom.plot([element],[-0.01],'o',color='black',alpha=0.5,mew=0)
             
@@ -260,23 +270,32 @@ def plotter():
     
     # f.3. closing the figure
     matplotlib.pyplot.tight_layout(pad=0.4,w_pad=0.5,h_pad=0)
-    matplotlib.pyplot.savefig('uvr.figure.pdf')
+    matplotlib.pyplot.savefig('{}.figure.pdf'.format(flag))
     matplotlib.pyplot.clf()
-
+    
     return top,bottom
 
-def uvrRespondingGenesReader():
+def selectedGeneSetReader(flag):
 
     '''
-    This function retrieves the names of UVR-responding genes.
+    This function retrieves the names of the selected gene set.
     '''
 
+    if flag == 'uvr':
+        fileName=uvrRespondingGenesFile
+    elif flag == 'ccm':
+        fileName=ccGenesFile
+    else:
+        print('error at selecting reading file. Exiting...')
+        sys.exit()
+        
     names=[]
-    with open(uvrRespondingGenesFile,'r') as f:
+    with open(fileName,'r') as f:
         next(f)
         for line in f:
             vector=line.split(',')
-            name='gene:Thaps{}'.format(vector[1])
+            geneID=int(vector[0].split('_')[1])
+            name='gene:Thaps{}'.format(geneID)
             names.append(name)
 
     names.sort()
@@ -291,6 +310,7 @@ def uvrRespondingGenesReader():
 expressionFile='/Volumes/omics4tb/alomana/projects/dtp/data/expression/tippingPoints/cufflinks/allSamples/genes.fpkm_table.v2.txt'
 metaDataFile='/Volumes/omics4tb/alomana/projects/dtp/data/expression/tippingPoints/metadata/metadata.v2.tsv'
 uvrRespondingGenesFile='/Volumes/omics4tb/alomana/projects/dtp/data/functionalAnnotation/UVR_Responsive_Genes.csv'
+ccGenesFile='/Volumes/omics4tb/alomana/projects/dtp/data/functionalAnnotation/CentralCarbonGenes_V2.csv'
 
 epochs=[0,1]
 growths=['exp','sta']
@@ -298,6 +318,11 @@ diurnals=['AM','PM']
 
 numberOfThreads=4
 crossValidationIterations=10000
+
+flag='ccm'
+#flag='uvr'
+
+plotting=True
 
 # 1. read data
 print('reading data...')
@@ -309,19 +334,26 @@ expression=library.expressionReader(expressionFile)
 sortedGeneNames=list(expression.keys())
 sortedGeneNames.sort()
 
-# 1.3. read UVR-responding genes
-uvrGenes=uvrRespondingGenesReader()
-print('\t found {} UVR response genes...'.format(len(uvrGenes)))
+# 1.3. read selected set of genes
+selectedSet=selectedGeneSetReader(flag)
+print('\t found {} {} genes...'.format(len(selectedSet),flag))
 
 # 2. perform analysis
 print('performing analysis...')
-effectsUVR,effectsNOUVR,effectsUVRdict=analysis(uvrGenes,verbose=True,plotting=False)
+effectsSS,effectsNOSS,effectsSSdict=analysis(selectedSet,plotting)
 
 # 3. plot one histogram for each set of genes, one for UVR-response genes, another for the rest
 print('plotting...')
 topThreshold,bottomThreshold=plotter()
 
-# 4. perform random sets of 101 genes and define the number of
+# 3.1. print significant effect genes
+numberOfDeviations=0
+for element in effectsSSdict:
+    if effectsSSdict[element] > topThreshold or effectsSSdict[element] < bottomThreshold:
+        print('\t significant deviation for {}\t{}'.format(element,effectsSSdict[element]))
+        numberOfDeviations=numberOfDeviations+1
+
+# 4. perform random sets of n genes and define the number of
 print('running cross-validation...')
 crossValidation()
 
